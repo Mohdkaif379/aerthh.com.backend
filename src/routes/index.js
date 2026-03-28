b0721c871cb5482c8052eaacf353b39aebd56c5c
Mohdkaif379 <mohdkaif8603@gmail.com>
2026-03-28 13:21:45 +0530

    updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const homeRoutes = require('./home/home');
 const authRoutes = require('./authuser/customer/auth');
 const buyRoutes = require('./products/buy');
 const checkoutRoutes = require('./checkout/checkout');
+const orderRoutes = require('./order/order');
 const router = express.Router();
 
 // Mount route modules
@@ -10,5 +11,6 @@ router.use('/', homeRoutes);
 router.use('/auth', authRoutes);
 router.use('/products', buyRoutes);
 router.use('/checkout', checkoutRoutes);
+router.use('/orders', orderRoutes);
 
 module.exports = router;
